3bc13afb3776c44fa6eccb91d9c4b8b1bd3febdf
YaqubAliy02 <yaqubaliy02@gmail.com>
2025-01-06 18:50:00 +0500

    ENDPOINT: Add GetMapById Endpoint

--- Minimal-Api/Endpoints/BookEndpoints.cs
+++ Minimal-Api/Endpoints/BookEndpoints.cs
@@ -10,6 +10,12 @@ namespace Minimal_Api.Endpoints
             app.MapGet("/api/books", async (BookDbContext context) =>
               await context.Books.ToListAsync());
 
+            app.MapGet("api/book/{Guid id}", async (BookDbContext context, Guid id) =>
+            {
+                var book = await context.Books.FindAsync(id);
+                return book is null ? Results.NotFound() : Results.Ok(book);
+
+            });
             return app;
         }
     }
